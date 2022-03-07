000b7ee4cb22ce5b59ec2051f463e9894d8675ce
M_Kazbek <blackcoresec@gmail.com>
2022-03-07 22:21:09 +0300

    Added "Book" entity

---
+++ migrations/Version20220307191707.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220307191707 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE book (id INT NOT NULL, name VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, cover TEXT NOT NULL, file TEXT NOT NULL, last_read_datetime TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, is_downloadable BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX name_author_unique ON book (name, author)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
+        $this->addSql('DROP TABLE book');
+    }
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(
+ *    name="book",
+ *    uniqueConstraints={
+ *        @ORM\UniqueConstraint(name="name_author_unique", columns={"name", "author"})
+ *    }
+ * )
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $cover;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $last_read_datetime;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_downloadable;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(string $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getLastReadDatetime(): ?\DateTimeInterface
+    {
+        return $this->last_read_datetime;
+    }
+
+    public function setLastReadDatetime(?\DateTimeInterface $last_read_datetime): self
+    {
+        $this->last_read_datetime = $last_read_datetime;
+
+        return $this;
+    }
+
+    public function getIsDownloadable(): ?bool
+    {
+        return $this->is_downloadable;
+    }
+
+    public function setIsDownloadable(bool $is_downloadable): self
+    {
+        $this->is_downloadable = $is_downloadable;
+
+        return $this;
+    }
+}
