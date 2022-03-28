b29f5f95e4cdde75b4183a4406358f02da7d3e9f
M_Kazbek <blackcoresec@gmail.com>
2022-03-28 13:52:39 +0300

    Refactored test files structure.

--- tests/App/Controller/Book/BookControllerTest.php
+++ tests/App/Controller/Book/BookControllerTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Tests\Book;
+namespace App\Tests\App\Controller\Book;
 
 use App\Entity\User;
 use App\Entity\Book;
--- tests/App/EventListener/BooksEventsListenerTest.php
+++ tests/App/EventListener/BooksEventsListenerTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Tests\Book;
+namespace App\Tests\App\EventListener;
 
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
